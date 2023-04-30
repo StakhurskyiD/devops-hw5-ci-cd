a39adba8f533447c6cf0ef2f7a37b12a61987a36
Dmytro Stakhurskyi <dstakhurskyi@gmail.com>
2023-04-30 11:14:25 -0700

    Test workflow launching

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ const server = http.createServer((req, res) => {
 const port = process.env.PORT || 80;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log('Test workflows');
 });
